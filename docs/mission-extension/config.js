e39013d8ed5156033a0182e9d4b493c21dcbf614
THXel <thxel1992@gmail.com>
2026-07-28 18:18:20 +0200

    Update public docs

--- docs/mission-extension/config.js
+++
@@ -1,9 +0,0 @@
-window.PINGULEXA_MISSION_EXTENSION_CONFIG = Object.freeze({
-  snapshotUrls: [
-    "https://thxel.github.io/fangame-hub-public/mission-extension/mission_snapshot.json",
-    "https://raw.githubusercontent.com/THXel/fangame-hub-public/main/docs/mission-extension/mission_snapshot.json"
-  ],
-  refreshSeconds: 30,
-  requestTimeoutMs: 9000,
-  hubUrl: "https://thxel.github.io/fangame-hub-public/"
-});
